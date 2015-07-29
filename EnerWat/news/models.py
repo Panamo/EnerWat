cfc99fd8c4b577e73988f9502dabb876ee785f08
Parham Alvani <parham.alvani@gmail.com>
2015-07-29 13:12:29 +0430

    News application created.

---
+++ EnerWat/news/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+# Create your models here.
+class News(models.Model):
+    subject = models.CharField(max_length=255)
+    date = models.DateField()
+    keyword = models.CharField(max_length=255)
+    content = models.TextField()
