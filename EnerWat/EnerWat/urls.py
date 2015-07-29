42addb33f9eecbf6910a0e728c213b745e55d92a
Parham Alvani <parham.alvani@gmail.com>
2015-07-29 13:12:49 +0430

    User urls improved.

--- EnerWat/EnerWat/urls.py
+++ EnerWat/EnerWat/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^signup/', 'user.views.signup'),
+    url(r'^login/', 'user.views.login')
 ]
