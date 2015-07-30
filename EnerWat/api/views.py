a947a05a85aca15135b5a376e0f9c56a789f179c
Parham Alvani <parham.alvani@gmail.com>
2015-07-30 09:16:12 +0430

    API encoding improved.

--- EnerWat/api/views.py
+++ EnerWat/api/views.py
@@ -1,17 +1,12 @@
-import json
-
 from django.http import HttpResponse
+from django.core import serializers
 
 from news.models import News
-from .encoders import NewsEncoder
 
 
 # Create your views here.
 def get_news(request, id):
     news = News.objects.filter(id__gt=id).order_by("date")
     response = HttpResponse(content_type='application/json')
-    result = []
-    for new in news:
-        result.append(NewsEncoder.encode(new))
-    response.write(json.dumps(result))
+    response.write(serializers.serialize('json', news))
     return response
