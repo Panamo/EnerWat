4138407feea22d9f4c4df1647665f2efe401630d
Parham Alvani <parham.alvani@gmail.com>
2015-07-30 18:06:47 +0430

    Help text added into User model.

--- EnerWat/user/models.py
+++ EnerWat/user/models.py
@@ -8,7 +8,9 @@ class User(AbstractUser):
     phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$',
                                  message="Phone number must be entered in the format: '+999999999'.")
     # Fields
-    phone_number = models.CharField(validators=[phone_regex, ], max_length=20, verbose_name='phone number')
+    phone_number = models.CharField(validators=[phone_regex, ], max_length=20, verbose_name='phone number',
+                                    help_text="Phone number must be entered in the format: '+999999999'."
+                                    )
     university = models.CharField(max_length=255, verbose_name='university')
     major = models.CharField(max_length=255, verbose_name='major')
 
