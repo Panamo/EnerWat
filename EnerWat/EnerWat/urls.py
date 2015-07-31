1b633fba70442d274af36dbab8a91d357b81c983
Parham Alvani <parham.alvani@gmail.com>
2015-07-31 18:59:44 +0430

    Simple news listing and detailing is added.

--- EnerWat/EnerWat/urls.py
+++ EnerWat/EnerWat/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from news.views import NewsDetailView, NewsListView
 
 urlpatterns = [
     # Examples:
@@ -11,5 +12,7 @@ urlpatterns = [
     url(r'^signup/', 'user.views.signup'),
     url(r'^login/', 'user.views.login'),
     url(r'^api/get-news/(?P<id>\d+)', 'api.views.get_news'),
+    url(r'^news/(?P<pk>\d+)', NewsDetailView.as_view(), name='news_detail'),
+    url(r'^news$', NewsListView.as_view(), name='news_list'),
     url(r'^contact', 'main.views.contact'),
 ]
--- EnerWat/main/views.py
+++ EnerWat/main/views.py
@@ -11,5 +11,4 @@ def main(request):
 
 
 def contact(request):
-	pass
-	
+    pass
