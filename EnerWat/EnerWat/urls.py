69501da4950b1a477d9f5813398de543c8b53602
Parham Alvani <parham.alvani@gmail.com>
2015-08-01 06:14:46 +0430

    User dummy portal is added.

--- EnerWat/EnerWat/urls.py
+++ EnerWat/EnerWat/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from django.contrib import admin
 from news.views import NewsDetailView, NewsListView
+from user.views import UserDetailView
 
 urlpatterns = [
     # Examples:
@@ -9,11 +10,19 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.main'),
+
+    # User views
     url(r'^signup/?', 'user.views.signup'),
     url(r'^login/?', 'user.views.login'),
+    url(r'^profile/(?P<pk>\d+)', UserDetailView.as_view(), name='user_detail'),
+
+    # API views
     url(r'^api/get-news/(?P<id>\d+)', 'api.views.get_news'),
+
+    # News views
     url(r'^news/(?P<pk>\d+)', NewsDetailView.as_view(), name='news_detail'),
     url(r'^news/?$', NewsListView.as_view(), name='news_list'),
+
     url(r'^contact/?', 'main.views.contact'),
     url(r'^submit-paper/?', 'user.views.paper'),
 ]
