eee65a1608e611e7504e93b815ea6aee2a7f19ab
Parham Alvani <parham.alvani@gmail.com>
2015-08-01 06:01:14 +0430

    URL regular expressions are improved.

--- EnerWat/EnerWat/urls.py
+++ EnerWat/EnerWat/urls.py
@@ -9,11 +9,11 @@ urlpatterns = [
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', 'main.views.main'),
-    url(r'^signup/', 'user.views.signup'),
-    url(r'^login/', 'user.views.login'),
+    url(r'^signup/?', 'user.views.signup'),
+    url(r'^login/?', 'user.views.login'),
     url(r'^api/get-news/(?P<id>\d+)', 'api.views.get_news'),
     url(r'^news/(?P<pk>\d+)', NewsDetailView.as_view(), name='news_detail'),
     url(r'^news/?$', NewsListView.as_view(), name='news_list'),
-    url(r'^contact', 'main.views.contact'),
-    url(r'^submit-paper', 'user.views.paper'),
+    url(r'^contact/?', 'main.views.contact'),
+    url(r'^submit-paper/?', 'user.views.paper'),
 ]
