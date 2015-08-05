db796492a38d874230c70ef209121ceb9815755a
Parham Alvani <parham.alvani@gmail.com>
2015-08-05 22:14:48 +0430

    User profile handling is improved.

--- EnerWat/user/models.py
+++ EnerWat/user/models.py
@@ -1,9 +1,11 @@
 from django.contrib.auth.models import AbstractUser
+from django.core.urlresolvers import reverse
 from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    def get_absolute_url(self):
+        return reverse('user_detail', args=[str(self.pk)])
 
 
 class Article(models.Model):
